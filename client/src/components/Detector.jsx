d2ca47813d8b445793fcdf2791d963718de58ac4
Gabriel Vieira Vidal <gabrielvieriavidal2005@gmail.com>
2023-11-04 17:07:57 -0300

    Alteração no arquivo Detector.jsx

--- client/src/components/Detector.jsx
+++ client/src/components/Detector.jsx
@@ -2,35 +2,23 @@ import { useEffect } from "react";
 import {
     ChainId,
     useNetworkMismatch,
-    useNetwork,
-    useChainId,
+    useSwitchChain,
     ConnectWallet,
-    useAddress
+    useAddress,
   } from "@thirdweb-dev/react";
-  
+
 export const AutoConnect = () => {
     const address = useAddress(); // Get connected wallet address
-    const [, switchNetwork] = useNetwork(); // Switch to desired chain
+    const switchChain = useSwitchChain(); // Switch to desired chain
     const isMismatched = useNetworkMismatch(); // Detect if user is connected to the wrong network
-  
+    
     useEffect(() => {
       // Check if the user is connected to the wrong network
       if (isMismatched) {
         // Prompt their wallet to switch networks
-        switchNetwork(ChainId.ArbitrumGoerli); // the chain you want here
+        switchChain(ChainId.ArbitrumGoerli); // the chain you want here
       }
     }, [address]); // This above block gets run every time "address" changes (e.g. when the user connects)
-  
+
     return <ConnectWallet/>
   };
-
-
-//   <CustomButton 
-// btnType="button"
-// title={address ? 'Create a campaign' : 'Connect'}
-// styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
-// handleClick={() => {
-//   if(address) navigate('create-campaign')
-//   else connect()
-// }}
-// />
