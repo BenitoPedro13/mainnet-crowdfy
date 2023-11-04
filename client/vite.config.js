ed6276f2da982fbbc749330b34d3f4004f9b805f
Benito <benitopedro21@outlook.com>
2023-11-04 11:04:43 -0300

    wip

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
+  // define: {
+  //   global: "globalThis",
+  //   "process.env": {},
+  // },
 });
