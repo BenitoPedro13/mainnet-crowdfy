2416c12ffc3e6f9f3e516e0a668e6e3f5062641c
Benito <benitopedro21@outlook.com>
2023-11-05 18:17:11 -0300

    Fix: progressBar bug

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -1,44 +1,77 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
-import { thirdweb } from '../assets';
-import { calculateBarPercentage, daysLeft } from '../utils';
+import { thirdweb } from "../assets";
+import { calculateBarPercentage, daysLeft } from "../utils";
 
-const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
-
+const FundCard = ({
+  owner,
+  title,
+  description,
+  target,
+  deadline,
+  amountCollected,
+  image,
+  handleClick,
+}) => {
   const remainingDays = daysLeft(deadline);
 
-  const [width, setWidth] = useState((calculateBarPercentage(target, amountCollected) / 100) * 364)
-  
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth((calculateBarPercentage(target, amountCollected) / 100) * 364);
+  }, [target, amountCollected]);
+
   return (
-    <div className="max-w-[398px] w-full rounded-[12px] bg-[#FFFFFF] cursor-pointer border-solid border shadow-md" onClick={handleClick}>
-      <img src={image} alt="fund" className="w-full h-[175px] object-cover rounded-b-none rounded-t-xl"/>
+    <div
+      className="max-w-[398px] w-full rounded-[12px] bg-[#FFFFFF] cursor-pointer border-solid border shadow-md"
+      onClick={handleClick}
+    >
+      <img
+        src={image}
+        alt="fund"
+        className="w-full h-[175px] object-cover rounded-b-none rounded-t-xl"
+      />
 
       <div className="flex items-center gap-[10px] px-4 mt-[20px]">
-          <div className="w-[41px] h-[41px] rounded-full flex justify-center items-center bg-[#13131a]">
-            <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain"/>
-          </div>
-          <p className="flex-1 font-jakarta font-medium text-[16px] text-[#475467] truncate"><span className="text-[#475467]">{owner}</span></p>
+        <div className="w-[41px] h-[41px] rounded-full flex justify-center items-center bg-[#13131a]">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-1/2 h-1/2 object-contain"
+          />
+        </div>
+        <p className="flex-1 font-jakarta font-medium text-[16px] text-[#475467] truncate">
+          <span className="text-[#475467]">{owner}</span>
+        </p>
       </div>
 
       <div className="flex flex-col p-4">
         <div className="flex flex-col gap-1">
-          <h3 className="font-inter font-semibold text-[24px] text-[#101828] text-left leading-[26px] truncate">{title}</h3>
-          <p className="font-jakarta font-normal text-[#475467] text-left leading-[18px] line-clamp-2 text-ellipsis">{description}</p>
+          <h3 className="font-inter font-semibold text-[24px] text-[#101828] text-left leading-[26px] truncate">
+            {title}
+          </h3>
+          <p className="font-jakarta font-normal text-[#475467] text-left leading-[18px] line-clamp-2 text-ellipsis">
+            {description}
+          </p>
         </div>
 
         <div className="relative w-full h-[5px] bg-[#ECFCCB] mt-4 rounded-[53px]">
-            <div className={`h-full max-w-full ${!width ? 'w-0': `w-[${width}px]`} bg-[#84CC16] rounded-[53px]`}></div>
+          <div
+            className={`h-full max-w-full bg-[#84CC16] rounded-[53px]`}
+            style={{ width: !width ? "0px" : `${width}px` }}
+          ></div>
         </div>
-        
 
         <div className="flex justify-between flex-wrap mt-4 gap-2">
           <h4 className="font-inter font-semibold text-[14px] text-[#1E293B] leading-[22px]">{`${amountCollected} / ${target} ETH`}</h4>
 
-          <h4 className="font-inter font-semibold text-[14px] text-[#1E293B] leading-[22px]">{((amountCollected / target) * 100).toFixed(1)}%</h4>
+          <h4 className="font-inter font-semibold text-[14px] text-[#1E293B] leading-[22px]">
+            {((amountCollected / target) * 100).toFixed(1)}%
+          </h4>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FundCard
+export default FundCard;
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -107,16 +107,16 @@ const CreateCampaign = () => {
                   <path
                     d="M12 2.5V22.5"
                     stroke="black"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M17 5.5H9.5C8.57174 5.5 7.6815 5.86875 7.02513 6.52513C6.36875 7.1815 6 8.07174 6 9C6 9.92826 6.36875 10.8185 7.02513 11.4749C7.6815 12.1313 8.57174 12.5 9.5 12.5H14.5C15.4283 12.5 16.3185 12.8687 16.9749 13.5251C17.6313 14.1815 18 15.0717 18 16C18 16.9283 17.6313 17.8185 16.9749 18.4749C16.3185 19.1313 15.4283 19.5 14.5 19.5H6"
                     stroke="black"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               </div>
